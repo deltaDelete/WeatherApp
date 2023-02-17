a5a56d5f57363fc95889fce514f95f92c8b6dc02
deltaDelete <samansy@yandex.ru>
2023-02-17 09:32:01 +0700

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace WeatherApp
+{
+    partial class WeatherForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.WeatherDataGrid = new System.Windows.Forms.DataGridView();
+            this.searchBox = new System.Windows.Forms.TextBox();
+            this.measureUnitsBox = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.WeatherDataGrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // WeatherDataGrid
+            // 
+            this.WeatherDataGrid.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.WeatherDataGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.WeatherDataGrid.Location = new System.Drawing.Point(12, 12);
+            this.WeatherDataGrid.Name = "WeatherDataGrid";
+            this.WeatherDataGrid.ReadOnly = true;
+            this.WeatherDataGrid.Size = new System.Drawing.Size(776, 196);
+            this.WeatherDataGrid.TabIndex = 0;
+            // 
+            // searchBox
+            // 
+            this.searchBox.Location = new System.Drawing.Point(103, 214);
+            this.searchBox.Name = "searchBox";
+            this.searchBox.Size = new System.Drawing.Size(100, 20);
+            this.searchBox.TabIndex = 1;
+            this.searchBox.TextChanged += new System.EventHandler(this.searchBox_TextChanged);
+            // 
+            // measureUnitsBox
+            // 
+            this.measureUnitsBox.FormattingEnabled = true;
+            this.measureUnitsBox.Location = new System.Drawing.Point(103, 240);
+            this.measureUnitsBox.Name = "measureUnitsBox";
+            this.measureUnitsBox.Size = new System.Drawing.Size(121, 21);
+            this.measureUnitsBox.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(58, 217);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Поиск";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(18, 243);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Ед.измерения";
+            // 
+            // WeatherForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.searchBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.measureUnitsBox);
+            this.Controls.Add(this.WeatherDataGrid);
+            this.Name = "WeatherForm";
+            this.Text = "Погода Хакасии";
+            this.Load += new System.EventHandler(this.WeatherForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.WeatherDataGrid)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView WeatherDataGrid;
+        private System.Windows.Forms.TextBox searchBox;
+        private System.Windows.Forms.ComboBox measureUnitsBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,38 @@
+﻿using System.Linq;
+using System.Windows.Forms;
+
+namespace WeatherApp
+{
+    public partial class WeatherForm : Form
+    {
+        private WeatherDatabase _weather;
+
+        public WeatherForm()
+        {
+            InitializeComponent();
+
+            _weather = new WeatherDatabase();
+            _weather.Initialize();
+
+            WeatherDataGrid.DataSource = _weather.Weathers.ToList();
+
+            measureUnitsBox.Items.Add(MeasureUnits.Celsius.ToString());
+            measureUnitsBox.Items.Add(MeasureUnits.Fahrenheit.ToString());
+            measureUnitsBox.Items.Add(MeasureUnits.Kelvin.ToString());
+        }
+
+        private void searchBox_TextChanged(object sender, System.EventArgs e) {
+            if (string.IsNullOrEmpty(searchBox.Text)) {
+                WeatherDataGrid.DataSource = _weather.Weathers.ToList();
+                return;
+            }
+            WeatherDataGrid.DataSource = _weather.Weathers
+                .Where(w => w.CityName.ToLower().Contains(searchBox.Text.ToLower()))
+                .ToList();
+        }
+
+        private void WeatherForm_Load(object sender, System.EventArgs e) {
+
+        }
+    }
+}
